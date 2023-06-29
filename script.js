0841dba896e41fd20851115b5a98eb4c17f24c0e
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-06-29 13:09:31 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+
+let req = new XMLHttpRequest();
+
+let baseTemp;
+let values = [];
+
+let xScale;
+let yScale;
+
+let width = 1200;
+let height = 600;
+let padding = 60;
+
+let canvas = d3.select("#canvas");
+canvas.attr("width",width);
+canvas.attr("height",height);
+
+let generateScales = () =>{
+
+}
+
+let drawCells = () =>{
+
+}
+
+let drawAxes = () =>{
+
+}
+
 
